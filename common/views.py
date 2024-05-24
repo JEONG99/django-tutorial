5b8d023cd8b2397c7a3510a917f2b120d61612e6
jeong99 <jbh990621@gmail.com>
2024-05-24 14:18:09 +0900

    server_error view 수정

--- common/views.py
+++ common/views.py
@@ -24,4 +24,4 @@ def page_not_found(request, exception):
     return render(request, 'common/404.html', {})
 
 def server_error(request, exception):
-    return render(request, 'common/500.html', {})
+    return render(request, 'common/500.html', {}, status=500)
