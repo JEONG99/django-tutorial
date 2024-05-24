ee636420759a6d972c39eabeae3d825fb24a6702
jeong99 <jbh990621@gmail.com>
2024-05-24 15:00:02 +0900

    ALLOWED_HOSTS 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['15.165.65.171']
+ALLOWED_HOSTS = ['15.165.65.171', 'jeong-pybo.p-e.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
